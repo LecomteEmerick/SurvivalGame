4742222a48d33cbc9185465b74ff7e8ddb51faa2
Emerick Lecomte <emerick.lecomte@gmail.com>
2014-08-31 16:52:08 +0200

    Change Mouse
    la souris suit bien le déplacement du curseur

--- Unity Development/Assets/Scripts/DeplacementFolder/FollowRotation.cs
+++ Unity Development/Assets/Scripts/DeplacementFolder/FollowRotation.cs
@@ -13,17 +13,19 @@ public class FollowRotation : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        Screen.showCursor = false;
         center = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        //Screen.lockCursor = true;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        var delta = (Input.mousePosition - center) / Screen.height;
+        var delta = (new Vector3(Input.GetAxis("Mouse X"),0,0) - center) / Screen.height;
         if (delta.x > declenchement)
             transform.Rotate(0, (delta.x - declenchement) * Time.deltaTime * sensitivity,0);
         if (delta.x < -declenchement)
             transform.Rotate(0, (delta.x + declenchement) * Time.deltaTime * sensitivity,0);
-
+        
     }
 }
