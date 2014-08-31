e355282e382b63194dd3654bb11573595a2ce413
Emerick Lecomte <emerick.lecomte@gmail.com>
2014-08-31 15:24:11 +0200

    first commit
    fisrt commit

---
+++ Assets/Scripts/DeplacementFolder/DeplacementScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeplacementScript : MonoBehaviour {
+
+    [SerializeField]
+    private Transform _player;
+
+    [SerializeField]
+    private float walkSpeed = 5.0f;
+
+    private bool playerWantToMoveUp = false;
+    private bool playerWantToMoveDown = false;
+    private bool playerWantToMoveLeft = false;
+    private bool playerWantToMoveRight = false;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+
+    void Update()
+    {
+        checkDeplacement();
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        deplacement();
+	}
+
+    public void deplacement()
+    {
+        if (playerWantToMoveUp)
+            _player.position += Camera.main.transform.forward *
+                    walkSpeed * Time.deltaTime;
+        if (playerWantToMoveDown)
+            _player.position += -Camera.main.transform.forward *
+                    walkSpeed * Time.deltaTime;
+        if (playerWantToMoveLeft)
+            _player.position += -Camera.main.transform.right *
+                    walkSpeed * Time.deltaTime;
+        if (playerWantToMoveRight)
+            _player.position += Camera.main.transform.right *
+                    walkSpeed * Time.deltaTime;
+    }
+
+    public void checkDeplacement()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.UpArrow))
+            playerWantToMoveUp = true;
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            playerWantToMoveDown = true;
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.LeftArrow))
+            playerWantToMoveLeft = true;
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            playerWantToMoveRight = true;
+        if (Input.GetKeyUp(KeyCode.Z) || Input.GetKeyUp(KeyCode.UpArrow))
+            playerWantToMoveUp = false;
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+            playerWantToMoveDown = false;
+        if (Input.GetKeyUp(KeyCode.Q) || Input.GetKeyUp(KeyCode.LeftArrow))
+            playerWantToMoveLeft = false;
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+            playerWantToMoveRight = false;
+    }
+}
---
+++ Assets/Scripts/DeplacementFolder/FollowRotation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowRotation : MonoBehaviour {
+
+    [SerializeField]
+    float declenchement = 0.1f;
+
+    [SerializeField]
+    float sensitivity = 400.0f;
+
+    private Vector3 center;
+
+	// Use this for initialization
+	void Start () {
+        center = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        var delta = (Input.mousePosition - center) / Screen.height;
+        if (delta.x > declenchement)
+            transform.Rotate(0, (delta.x - declenchement) * Time.deltaTime * sensitivity,0);
+        if (delta.x < -declenchement)
+            transform.Rotate(0, (delta.x + declenchement) * Time.deltaTime * sensitivity,0);
+
+    }
+}
