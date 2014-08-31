7bfe01cae6a38dc64646b87fd16ed3717dbd342f
Emerick Lecomte <emerick.lecomte@gmail.com>
2014-08-31 17:34:51 +0200

    Ajout Menu
    Ajout des menus de début et in game pour lancer / quitter le jeu

---
+++ Unity Development/Assets/Scripts/MenuScript/MainMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+    private Rect centerRef;
+    private Rect afficheRect;
+
+    [SerializeField]
+    private GUITexture background;
+
+    // Use this for initialization
+    void Start()
+    {
+        background.pixelInset = new Rect(-Screen.width / 2, -Screen.height / 2, Screen.width, Screen.height);
+        centerRef = new Rect(Screen.width / 3, Screen.height / 3, Screen.width / 3, 30);
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        afficheRect = centerRef;
+        if (GUI.Button(afficheRect, "Play"))
+            Application.LoadLevel("TestScene");
+        afficheRect = verticalDown(afficheRect);
+        if (GUI.Button(afficheRect, "Exit"))
+            Application.Quit();
+    }
+
+    private Rect verticalDown(Rect afficheRect)
+    {
+        afficheRect.y += afficheRect.height + 10;
+        return afficheRect;
+    }
+}
---
+++ Unity Development/Assets/Scripts/MenuScript/MenuIngameScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuIngameScript : MonoBehaviour {
+
+    private bool afficheMenu = false;
+
+    private Rect centerRef;
+    private Rect afficheRect;
+
+	// Use this for initialization
+	void Start () {
+        centerRef = new Rect(Screen.width / 3, Screen.height / 3, Screen.width / 3, 30);
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            afficheMenu = !afficheMenu;
+        if (!afficheMenu && !Screen.lockCursor)
+            Screen.lockCursor = true;
+    }
+
+	// Update is called once per frame
+	void OnGUI () {
+	    if(afficheMenu)
+        {
+            if (Screen.lockCursor)
+                Screen.lockCursor = false;
+            afficheRect = centerRef;
+            if (GUI.Button(afficheRect, "Return to game"))
+                afficheMenu = false;
+            afficheRect = verticalDown(afficheRect);
+            if (GUI.Button(afficheRect, "Exit Game"))
+                Application.Quit();
+        }
+	}
+
+    private Rect verticalDown(Rect afficheRect)
+    {
+        afficheRect.y += afficheRect.height + 10;
+        return afficheRect;
+    }
+}
